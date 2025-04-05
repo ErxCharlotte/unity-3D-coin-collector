a6c7a440cf84c28e612f4834f25d543b8c437ad1
ErxCharlotte <878658607@qq.com>
2025-04-05 16:13:38 +1100

    Final

---
+++ Assets/newplayer.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class newplayer : MonoBehaviour {
+
+	private int count;
+	public Text countText;
+	public Text winText;
+	public Text Story1;
+
+
+	// Use this for initialization
+	void Start () {
+
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+		Story1.text = "Follow the pink road and collect the sweet things on purple brick.";
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter ( Collider other ) {
+
+
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+
+		if (other.gameObject.CompareTag ("Red Pick Up")) {
+			other.gameObject.SetActive (false);
+			count = count - 1;
+			SetCountText ();
+		}
+
+
+		if (other.gameObject.CompareTag ("Land Mine")) {
+			SceneManager.LoadScene ("Main Menu");
+		}
+
+		if (other.gameObject.CompareTag ("Win")) {
+			SceneManager.LoadScene ("Win");
+		}
+
+	}
+
+	void SetCountText () {
+		countText.text = "Sweet gifts:" + count.ToString ();
+
+		if (count == 1) {
+			Story1.text = "Good! Keep doing that and don't pick the things on red brick!";
+		}
+
+		if (count >= 4) {
+			winText.text = "Great!! You have successfully collected sweet gifts for the children, and you can collect more!";
+		}
+	}
+		
+}
---
+++ Assets/player1A.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class player1A : MonoBehaviour {
+
+	private int count;
+	public Text countText;
+	public Text winText;
+	public Text Story1;
+
+
+	// Use this for initialization
+	void Start () {
+
+		count = 0;
+		SetCountText ();
+		winText.text = "You Win!!!";
+		Story1.text = "Follow the red brick road and collect the coins";
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter ( Collider other ) {
+
+
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count = count + 5;
+			SetCountText ();
+		}
+
+		if (other.gameObject.CompareTag ("Red Pick Up")) {
+			other.gameObject.SetActive (false);
+			count = count - 5;
+			SetCountText ();
+		}
+
+
+		if (other.gameObject.CompareTag ("Land Mine")) {
+			SceneManager.LoadScene ("Main Menu");
+		}
+
+	}
+
+	void SetCountText () {
+		countText.text = "Coins: " + count.ToString ();
+
+		if (count == 5) {
+			Story1.text = "";
+		}
+
+		if (count >= 30) {
+			winText.text = "Awesome Babe. You Win!";
+		}
+	}
+
+}
